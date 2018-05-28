98706d34709f1534c3500abeb648e243c1b0baf0
MAKOSCAFEE <barnabasmakonda@gmail.com>
2018-05-28 12:20:07 +0300

    remove rxjs-compact

--- src/map/store/effects/dataSelection.effect.ts
+++ src/map/store/effects/dataSelection.effect.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
-import { of ,  Observable } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as dataSelectionAction from '../actions/data-selection.action';
 import * as visualizationObjectActions from '../actions/visualization-object.action';
 import * as fromServices from '../../services';
-import { tap ,  map, switchMap, catchError, combineLatest } from 'rxjs/operators';
-
+import { tap, map, switchMap, catchError, combineLatest } from 'rxjs/operators';
 
 @Injectable()
 export class DataSelectionEffects {
-  constructor(
-    private actions$: Actions,
-    private geofeatureService: fromServices.GeoFeatureService
-  ) {}
+  constructor(private actions$: Actions, private geofeatureService: fromServices.GeoFeatureService) {}
 
   @Effect({ dispatch: false })
   updatePe$ = this.actions$.ofType(dataSelectionAction.UPDATE_PE_SELECTION).pipe(
