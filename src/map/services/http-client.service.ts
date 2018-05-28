24e5bc9e25b26ca5e3cf7b5ce29743dd2e847ca0
MAKOSCAFEE <barnabasmakonda@gmail.com>
2018-05-28 12:05:34 +0300

    fix data filter component failed to load

--- src/map/services/http-client.service.ts
+++ src/map/services/http-client.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ManifestService } from './manifest.service';
-import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { throwError } from 'rxjs';
 import { catchError, mergeMap } from 'rxjs/operators';
 
 @Injectable()
@@ -24,9 +24,7 @@ export class HttpClientService {
     const rootUrlPromise = useRootUrl ? this._getRootUrl() : this._getApiRootUrl();
 
     return rootUrlPromise.pipe(
-      mergeMap(rootUrl =>
-        this.httpClient.post(rootUrl + url, data).pipe(catchError(this._handleError))
-      )
+      mergeMap(rootUrl => this.httpClient.post(rootUrl + url, data).pipe(catchError(this._handleError)))
     );
   }
 
@@ -34,9 +32,7 @@ export class HttpClientService {
     const rootUrlPromise = useRootUrl ? this._getRootUrl() : this._getApiRootUrl();
 
     return rootUrlPromise.pipe(
-      mergeMap(rootUrl =>
-        this.httpClient.put(rootUrl + url, data).pipe(catchError(this._handleError))
-      )
+      mergeMap(rootUrl => this.httpClient.put(rootUrl + url, data).pipe(catchError(this._handleError)))
     );
   }
 
@@ -68,7 +64,7 @@ export class HttpClientService {
         statusText: err.statusText
       };
     }
-    return new ErrorObservable(error);
+    return throwError(error);
   }
 
   /**
